56bad5bfe92bc0dd1d4bc0d81c2a2a6dbbfa641c
michaelpm54 <39412876+michaelpm54@users.noreply.github.com>
2020-11-08 10:38:19 +0000

    Remove some unused constants

--- src/constants.h
+++ src/constants.h
@@ -1,15 +1,11 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-constexpr int kScale {4};
-
 constexpr int kLcdWidth {160};
 constexpr int kLcdHeight {144};
 
 constexpr int kMapWidth {32};
 constexpr int kMapHeight {64};
-
-constexpr float kClearColor[3] {48 / 255.0f, 146 / 255.0f, 153 / 255.0f};
 
 constexpr int kCpuFreq {4'194'304};
 constexpr int kMaxCyclesPerFrame {kCpuFreq / 60};
