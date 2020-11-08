ca8477430d2aa025c2a22343ac0d657876f69dfe
michaelpm54 <39412876+michaelpm54@users.noreply.github.com>
2020-11-08 16:45:34 +0000

    More pleasant colours

--- assets/shaders/quad.frag.glsl
+++ assets/shaders/quad.frag.glsl
@@ -4,7 +4,7 @@ out vec4 colour;
 uniform usampler2D tex;
 void main()
 {
-	float c = texture(tex, texCoord).r / 255.0f;
+    float c = texture(tex, texCoord).r / 255.0f;
 
-	colour = vec4(0.4, c, 0.4, 1);
+    colour = vec4(0.2 + c * 2, 0.3 + c * 2, 0.5 + c, 1);
 }
