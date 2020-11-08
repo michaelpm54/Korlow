8ffe9efdb4012846854071da64fd8dbbf8d88f46
michaelpm54 <39412876+michaelpm54@users.noreply.github.com>
2020-11-08 12:01:38 +0000

    Fix conversion warning

--- src/main.cpp
+++ src/main.cpp
@@ -208,7 +208,7 @@ int main(int argc, char* argv[])
             ImGui::Begin("Map", nullptr, ImGuiWindowFlags_NoResize);
             ImVec2 uv_min {0.0f, 0.0f};
             ImVec2 uv_max {1.0f, 1.0f};
-            ImGui::Image((ImTextureID)map.handle, ImVec2(400.0f, 400.0f), uv_min, uv_max);
+            ImGui::Image(reinterpret_cast<ImTextureID>(static_cast<intptr_t>(map.handle)), ImVec2(400.0f, 400.0f), uv_min, uv_max);
             ImGui::End();
         }
 
